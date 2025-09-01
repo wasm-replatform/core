48a760d8ded5c51ed3cca1f64eefe5629f0415f2
andrew_weston <andrew.weston@propellerhead.co.nz>
2025-09-02 10:54:53 +1200

    add metrics layer to tracing

--- telemetry/src/init.rs
+++ telemetry/src/init.rs
@@ -1,8 +1,9 @@
-//! # Initializer
+//! # Telemetry
 //!
 //! Initialize the OpenTelemetry collectors and exporters.
 
 use std::env;
+use std::sync::OnceLock;
 
 use anyhow::Result;
 use opentelemetry::trace::TracerProvider;
@@ -11,84 +12,136 @@ use opentelemetry_otlp::{MetricExporter, SpanExporter, WithExportConfig};
 use opentelemetry_sdk::Resource;
 use opentelemetry_sdk::metrics::SdkMeterProvider;
 use opentelemetry_sdk::trace::SdkTracerProvider;
-use serde::Deserialize;
-use tracing_subscriber::fmt::format::FmtSpan;
+use tracing_opentelemetry::MetricsLayer;
 use tracing_subscriber::layer::SubscriberExt;
+use tracing_subscriber::util::SubscriberInitExt;
 use tracing_subscriber::{EnvFilter, Registry};
 
-/// Initialize OpenTelemetry with the provided configuration.
-///
-/// # Errors
-///
-/// Returns an error if the telemetry system fails to initialize, such as if
-/// the OpenTelemetry exporter cannot be created or if setting the global
-/// subscriber fails.
-pub fn init(config: &Config) -> Result<()> {
-    let resource = Resource::from(config.clone());
+static RESOURCE: OnceLock<Resource> = OnceLock::new();
 
-    // metrics
-    let meter_provider = init_metrics(config, resource.clone())?;
-    global::set_meter_provider(meter_provider);
+/// Telemetry initializer.
+pub struct Telemetry {
+    /// The name of the application to for the purposes of identifying the
+    /// service in telemetry data.
+    app_name: String,
 
-    // tracer
-    let tracer_provider = init_traces(config, resource)?;
-    global::set_tracer_provider(tracer_provider.clone());
+    /// The name of the environment, e.g. "production", "staging", "development".
+    env_name: Option<String>,
 
-    // tracing
-    let env_filter = EnvFilter::from_default_env();
-    let fmt_layer =
-        tracing_subscriber::fmt::layer().with_span_events(FmtSpan::NEW | FmtSpan::CLOSE);
-    let tracer = tracer_provider.tracer(config.app_name.clone());
-    let tracing_layer = tracing_opentelemetry::layer().with_tracer(tracer);
-
-    let subscriber = Registry::default().with(env_filter).with(fmt_layer).with(tracing_layer);
-    tracing::subscriber::set_global_default(subscriber)?;
-
-    Ok(())
+    /// The OpenTelemetry metrics collection endpoint.
+    endpoint: Option<String>,
 }
 
-fn init_traces(config: &Config, resource: Resource) -> Result<SdkTracerProvider> {
+impl Telemetry {
+    /// Create a new telemetry resource.
+    #[must_use]
+    pub fn new(name: impl Into<String>) -> Self {
+        Self {
+            app_name: name.into(),
+            env_name: None,
+            endpoint: None,
+        }
+    }
+
+    /// Override the default app name.
+    #[must_use]
+    pub fn name(mut self, name: impl Into<String>) -> Self {
+        self.app_name = name.into();
+        self
+    }
+
+    /// Set the environment name.
+    #[must_use]
+    pub fn env(mut self, env_name: impl Into<String>) -> Self {
+        self.env_name = Some(env_name.into());
+        self
+    }
+
+    /// Set the OpenTelemetry endpoint.
+    #[must_use]
+    pub fn endpoint(mut self, endpoint: impl Into<String>) -> Self {
+        self.endpoint = Some(endpoint.into());
+        self
+    }
+
+    /// Initializes telemetry using the provided configuration.
+    ///
+    /// # Errors
+    ///
+    /// Returns an error if the telemetry system fails to initialize, such as if
+    /// the OpenTelemetry exporter cannot be created or if setting the global
+    /// subscriber fails.
+    pub fn build(self) -> Result<()> {
+        let resource = Resource::from(&self);
+        RESOURCE.set(resource).expect("Resource already set");
+
+        // metrics
+        let meter_provider = init_metrics(self.endpoint.as_deref())?;
+        global::set_meter_provider(meter_provider.clone());
+
+        // tracing
+        let tracer_provider = init_traces(self.endpoint.as_deref())?;
+        global::set_tracer_provider(tracer_provider.clone());
+
+        let filter_layer = EnvFilter::from_default_env()
+            .add_directive("hyper=off".parse()?)
+            .add_directive("h2=off".parse()?)
+            .add_directive("tonic=off".parse()?);
+        // let fmt_layer =
+        //     tracing_subscriber::fmt::layer().with_span_events(FmtSpan::NEW | FmtSpan::CLOSE);
+        let tracer = tracer_provider.tracer(self.app_name);
+        let tracing_layer = tracing_opentelemetry::layer().with_tracer(tracer);
+        let metrics_layer = MetricsLayer::new(meter_provider);
+
+        // set global default subscriber
+        Registry::default()
+            .with(filter_layer)
+            .with(tracing_layer)
+            .with(metrics_layer)
+            .try_init()?;
+
+        Ok(())
+    }
+}
+
+fn init_traces(endpoint: Option<&str>) -> Result<SdkTracerProvider> {
     let mut builder = SpanExporter::builder().with_tonic();
-    if let Some(endpoint) = &config.endpoint {
+    if let Some(endpoint) = endpoint {
         builder = builder.with_endpoint(endpoint);
     }
     let exporter = builder.build()?;
+    let resource = RESOURCE.wait().clone();
+
     Ok(SdkTracerProvider::builder().with_resource(resource).with_batch_exporter(exporter).build())
 }
 
-fn init_metrics(config: &Config, resource: Resource) -> Result<SdkMeterProvider> {
+fn init_metrics(endpoint: Option<&str>) -> Result<SdkMeterProvider> {
     let mut builder = MetricExporter::builder().with_tonic();
-    if let Some(endpoint) = &config.endpoint {
+    if let Some(endpoint) = endpoint {
         builder = builder.with_endpoint(endpoint);
     }
     let exporter = builder.build()?;
+    let resource = RESOURCE.wait().clone();
+
     Ok(SdkMeterProvider::builder().with_resource(resource).with_periodic_exporter(exporter).build())
 }
 
-/// Telemetry configuration.
-#[derive(Deserialize, Debug, Clone)]
-pub struct Config {
-    /// The name of the application to for the purposes of identifying the
-    /// service in telemetry data.
-    pub app_name: String,
-
-    /// The name of the environment, e.g. "production", "staging", "development".
-    pub env_name: Option<String>,
-
-    /// The OpenTelemetry metrics collection endpoint.
-    pub endpoint: Option<String>,
+/// Returns a reference to the OpenTelemetry [`Resource`] used to initialize
+/// telemetry for a service.
+pub fn resource() -> &'static Resource {
+    RESOURCE.get().expect("Resource not set")
 }
 
-impl From<Config> for Resource {
-    fn from(config: Config) -> Self {
+impl From<&Telemetry> for Resource {
+    fn from(otel: &Telemetry) -> Self {
         Self::builder()
-            .with_service_name(config.app_name.clone())
+            .with_service_name(otel.app_name.clone())
             .with_attributes(vec![
                 KeyValue::new(
                     "deployment.environment",
-                    config.env_name.clone().unwrap_or_else(|| "unknown".to_string()),
+                    otel.env_name.clone().unwrap_or_else(|| "unknown".to_string()),
                 ),
-                KeyValue::new("service.namespace", config.app_name),
+                KeyValue::new("service.namespace", otel.app_name.clone()),
                 KeyValue::new("service.version", env!("CARGO_PKG_VERSION")),
                 KeyValue::new(
                     "service.instance.id",
