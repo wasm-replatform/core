6367a71789fa28f17b9b0a5d834d8d93a0de7109
Moritz Drexl <moritz.drexl@propellerhead.co.nz>
2025-09-19 10:53:07 +0200

    improve error handling and docs

--- keyvault/src/lib.rs
+++ keyvault/src/lib.rs
@@ -1,15 +1,33 @@
 use std::sync::Arc;
+use std::time::Duration;
 
-use anyhow::{Result, bail};
+use azure_core::http::StatusCode;
 use azure_identity::DeveloperToolsCredential;
 use azure_security_keyvault_secrets::SecretClient;
 use tokio::sync::watch;
+use tokio::time;
+
+#[derive(Debug)]
+pub enum KeyVaultError {
+    AzureError(azure_core::Error),
+    SecretMissing,
+}
+
+impl From<azure_core::Error> for KeyVaultError {
+    fn from(error: azure_core::Error) -> Self {
+        Self::AzureError(error)
+    }
+}
+
+pub type Result<T> = std::result::Result<T, KeyVaultError>;
 
 pub struct SecretRetriever {
     secret_client: Arc<SecretClient>,
 }
 
 impl SecretRetriever {
+    /// Create a new `SecretRetriever` from a keyvault name. Only the vault name is required, not
+    /// the full url.
     pub fn new(keyvault: &str) -> Result<SecretRetriever> {
         let credential = DeveloperToolsCredential::new(None)?;
         Ok(SecretRetriever {
@@ -21,24 +39,41 @@ impl SecretRetriever {
         })
     }
 
-    /// Returns a watchable secret. Only returns after the initial secret has been fetched.
-    pub async fn watch_secret(&self, secret_name: &str) -> Watcher {
-        let secret = fetch_secret_retrying(&self.secret_client, secret_name).await;
+    /// Returns a subscriber that can wait for secret changes. Only returns after the initial
+    /// secret has been fetched.
+    ///
+    /// Starts a background watch task that periodically polls for secret updates. If a new,
+    /// different secret is found, the subscriber will be notified.
+    pub async fn watch_secret(&self, secret_name: &str) -> Result<Subscriber> {
+        let secret = fetch_secret_retrying(&self.secret_client, secret_name).await?;
         let (tx, mut rx) = watch::channel(secret.clone());
         rx.mark_changed();
 
         let secret_client = self.secret_client.clone();
         let secret_name = secret_name.to_string();
+
+        let watcher_tx = tx.clone();
         tokio::spawn(async move {
             let mut last_secret = secret;
+            let mut interval = time::interval(Duration::from_hours(1));
             loop {
-                let secret = fetch_secret_retrying(&secret_client, &secret_name).await;
+                interval.tick().await;
+                let secret = match fetch_secret_retrying(&secret_client, &secret_name).await {
+                    Ok(secret) => secret,
+                    Err(error) => {
+                        tracing::error!(
+                            ?error,
+                            "Unrecoverable secret fetching error, stopping watcher task"
+                        );
+                        break;
+                    }
+                };
                 if secret != last_secret {
                     last_secret = secret.clone();
-                    match tx.send(secret) {
+                    match watcher_tx.send(secret) {
                         Ok(()) => {}
                         Err(watch::error::SendError(_)) => {
-                            println!("Watcher dropped, stopping watcher thread");
+                            tracing::error!("Watch subscriber dropped, stopping watcher thread");
                             break;
                         }
                     }
@@ -46,44 +81,66 @@ impl SecretRetriever {
             }
         });
 
-        Watcher { rx }
+        Ok(Subscriber { tx, rx })
     }
 }
 
-pub struct Watcher {
+pub struct Subscriber {
+    /// Only used for generating new subscribers when cloning.
+    tx: watch::Sender<String>,
     rx: watch::Receiver<String>,
 }
 
-impl Watcher {
-    /// Waits until a new secret value is available.
+impl Clone for Subscriber {
+    fn clone(&self) -> Self {
+        Subscriber { tx: self.tx.clone(), rx: self.tx.subscribe() }
+    }
+}
+
+impl Subscriber {
+    /// Waits until a new secret value is available and returns it.
     ///
-    /// Returns `None` if the watcher has been dropped.
+    /// Returns `None` if the watcher thread has been stopped.
     pub async fn next(&mut self) -> Option<String> {
         self.rx.changed().await.ok()?;
         Some(self.rx.borrow_and_update().to_string())
     }
 }
 
-async fn fetch_secret_retrying(secret_client: &SecretClient, secret_name: &str) -> String {
+/// Retries intermittent errors, but fails with unrecoverable errors like access denied.
+async fn fetch_secret_retrying(secret_client: &SecretClient, secret_name: &str) -> Result<String> {
+    let mut interval = time::interval(Duration::from_secs(3));
     loop {
+        interval.tick().await;
         match fetch_secret(secret_client, secret_name).await {
-            Ok(secret) => {
-                return secret;
+            Ok(Some(secret)) => {
+                return Ok(secret);
             }
-            Err(e) => {
-                println!("Error fetching secret: {e}. Retrying");
-                tokio::time::sleep(std::time::Duration::from_secs(1)).await;
+            Ok(None) => {
+                return Err(KeyVaultError::SecretMissing);
             }
+            Err(e) => match e.kind() {
+                azure_core::error::ErrorKind::HttpResponse {
+                    status: StatusCode::Forbidden,
+                    error_code: _,
+                    raw_response: _,
+                } => {
+                    return Err(e)?;
+                }
+                _ => {
+                    tracing::warn!("Error fetching secret: {e:#?}. Retrying");
+                    interval.tick().await;
+                }
+            },
         }
     }
 }
 
-async fn fetch_secret(secret_client: &SecretClient, secret_name: &str) -> Result<String> {
+async fn fetch_secret(
+    secret_client: &SecretClient, secret_name: &str,
+) -> azure_core::Result<Option<String>> {
     let secret = secret_client.get_secret(secret_name, None).await?.into_body().await?;
-    match secret.value {
-        Some(value) => Ok(value),
-        None => bail!("secret value missing"),
-    }
+    Ok(secret.value)
 }
 
 #[cfg(test)]
@@ -92,8 +149,8 @@ mod tests {
 
     #[tokio::test]
     async fn test_watch_secret() -> Result<()> {
-        let retriever = SecretRetriever::new("test")?;
-        let mut watcher = retriever.watch_secret("test").await;
+        let retriever = SecretRetriever::new("kv-ae-realtime-t01")?;
+        let mut watcher = retriever.watch_secret("realtime-confluent-key").await?;
         assert_eq!(watcher.next().await.unwrap(), "secret");
         Ok(())
     }
