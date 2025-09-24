7f824eaa9eb7d9752a2b4548ef72b334ab6a250e
Moritz Drexl <moritz.drexl@propellerhead.co.nz>
2025-09-24 08:25:55 +0200

    WIP

--- messaging/src/handler_driver.rs
+++ messaging/src/handler_driver.rs
@@ -1,6 +1,8 @@
 use api::Handler;
 
-use crate::{KafkaConfig, Message};
+use crate::consumer::KafkaConsumer;
+use crate::producer::KafkaProducer;
+use crate::{KafkaConfig, Message, MessagingError};
 
 pub struct HandlerDriver {
     config: KafkaConfig,
@@ -11,11 +13,28 @@ impl HandlerDriver {
         Self { config }
     }
 
-    pub fn run<I, O, P>(&self, provider: P)
+    pub async fn run<I, O, P>(
+        &self, provider: P, from_message: impl Fn(Message) -> I + Clone + Send + Sync,
+        to_message: impl Fn(O) -> Vec<Message> + Send + Sync,
+    ) -> Result<(), MessagingError>
     where
-        I: Handler<Vec<O>, P>,
-        I: From<Message>,
-        O: Into<Message>,
+        I: Handler<O, P>,
+        P: Send + Sync + 'static,
+        <I as Handler<O, P>>::Error: std::fmt::Debug,
     {
+        let consumer = KafkaConsumer::new(&self.config, None)?;
+        let producer = KafkaProducer::new(&self.config, None)?;
+
+        consumer.start(&["foo"], |message| async move {
+            let input = from_message(message);
+            let output = input.handle("", &provider).await.unwrap();
+            let msgs = to_message(output.body);
+            for msg in msgs {
+                producer.send_once(msg).await?;
+            }
+            Ok(())
+        });
+
+        Ok(())
     }
 }
